d6e5da1c34e9b2ebe87cd37037ce1ce28c35c666
bobzz-zone <bobzz.zone@gmail.com>
2017-08-28 19:53:07 +0700

    a

--- events/contribution/report/contribution_summary_report/contribution_summary_report.js
+++ events/contribution/report/contribution_summary_report/contribution_summary_report.js
@@ -3,6 +3,21 @@
 
 frappe.query_reports["Contribution Summary Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+			"reqd": 1,
+			"width": "60px"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "60px"
+		},
 	]
 }
--- events/contribution/report/contribution_summary_report/contribution_summary_report.py
+++ events/contribution/report/contribution_summary_report/contribution_summary_report.py
@@ -3,7 +3,20 @@
 
 from __future__ import unicode_literals
 import frappe
-
+from frappe.utils import flt
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = ["Item:link/Item:200","Total Qty:Float:100","Qty Percent:Percent:100","Total Sales:Currency:200","Sales Percent:Percent:100","Total Cost:Currency:200","Cost Percent:Percent:100"], []
+	result = frappe.db.sql("""select cr.item_code , sum(cr.amount) , sum(cr.qty),sum(cr.cost) 
+		from `tabContribution Result` cr join `tabContribution Tool` p on cr.parent=p.name
+		where p.docstatus=1 and (date between "{}" and "{}") group by cr.item_code 
+		""".format(filters.get("from_date"),filters.get("to_date")),as_list=1)
+	total_cost=0
+	total_qty=0
+	total_sales=0
+	for row in result:
+		total_cost=flt(row[3])
+		total_qty=flt(row[2])
+		total_sales=flt(row[1])
+	for row in result:
+		data.append([row[0],row[1],((flt(row[1])/total_qty)*100),row[2],((flt(row[2])/total_sales)*100),row[3],((flt(row[3])/total_cost)*100)])
 	return columns, data
